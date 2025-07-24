9c87253dee3a03e080df417ed42180db0b75f6b3
Miguel Viana <miguelpinxs@gmail.com>
2025-07-24 14:57:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/sala_leitura.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sala_leitura.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
